682e4604e8d4995152d868db26703de4706f4055
Gerren <g.ballard23@outlook.com>
2021-09-01 19:33:22 -0500

    commit tree 2c2fcb70073d985009f975a3a174a1a7ace34f14

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(array){
+  //  let noWin = undefined
+   const winningGame = array.find(game => game.result === 'W')
+   if(winningGame){
+       return winningGame.year
+   }else{
+       return undefined
+   }
+   
+    
+    //for(let item of array){
+      //  if(item.result === 'W'){
+            //return record.year
+       // }
+   // }
+}
 
